acaa280aed80a3e8ff35dd8dab95676b80d17666
aulaenlanube <tpalaciosayala@gmail.com>
2024-03-28 17:52:25 -0500

    video 9 curso gas

--- funciones.js
+++ funciones.js
@@ -6,3 +6,9 @@ function doGet() {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();  
   }
   
+function obtenerContactos()
+{
+    let hoja=SpreadsheetApp.openById('1rMEv3L33ycZV2uf5_6MaDiMtrf2d5fVhPxYXCW8LYBk').getActiveSheet();
+    let datos=hoja.getDataRange().getValues();
+    return datos;
+}
