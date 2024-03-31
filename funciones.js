cded3c57df0abb18686313276a6e8c0ec7ef8dbb
aulaenlanube <tpalaciosayala@gmail.com>
2024-03-31 11:59:44 -0500

    video15.gas avanzado

--- funciones.js
+++ funciones.js
@@ -1,14 +1,29 @@
-function doGet() {
-    return HtmlService.createTemplateFromFile("web").evaluate().setTitle("agenda-googleas");
-  }
-  
-  function obtenerDatosHTML(nombre) {
-    return HtmlService.createHtmlOutputFromFile(nombre).getContent();  
-  }
-  
-function obtenerContactos()
+
+const HOJA = SpreadsheetApp.openById('1rMEv3L33ycZV2uf5_6MaDiMtrf2d5fVhPxYXCW8LYBk').getActiveSheet();
+
+function doGet(datos)
 {
-    let hoja=SpreadsheetApp.openById('1rMEv3L33ycZV2uf5_6MaDiMtrf2d5fVhPxYXCW8LYBk').getActiveSheet();
-    let datos=hoja.getDataRange().getValues();
-    return datos;
+  insertarContacto(datos.parameter.nombre, datos.parameter.correo);
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Script');
+}
+
+function doPost(datos)
+{
+    insertarContacto(datos.parameter.nombre, datos.parameter.correo);
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Script');
+}
+
+function obtenerDatosHTML(nombre)
+{
+    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+}
+
+function obtenerContactos() 
+{
+    return HOJA.getDataRange().getValues();
+}
+
+function insertarContacto(nombre, correo)
+{
+    HOJA.appendRow([nombre,correo]);
 }
